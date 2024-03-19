0e2f20a4598bde5ccdfe3c6e3e50db5ab6ddb462
Sapphirestar <119770319+Goldenglows@users.noreply.github.com>
2024-03-19 12:09:51 +0800

    Add files via upload

---
+++ Day6-11.cpp
@@ -0,0 +1,30 @@
+#define _CRT_SECURE_NO_WARNINGS
+//#include<stdio.h>
+//#include<stdlib.h>
+//#include<math.h>
+//#include<string.h>
+
+#include<iostream>
+#include<math.h>
+#include<string.h>
+using namespace std;
+
+int M, N, flag;
+int arr[10] = { 0,0,0,0,0,0,0,0,0,0 };
+
+int main() {
+	cin >> M >> N;
+	for (int j = M; j <= N; j++) {
+		int i = j;
+		while (i > 0) {
+			flag = i % 10;
+			arr[flag]++;
+			i /= 10;
+		}
+	}
+
+	for (int i = 0; i <= 9; i++)
+		cout << arr[i] << " ";
+
+	return 0;
+}
