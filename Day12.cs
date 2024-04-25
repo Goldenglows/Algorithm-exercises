791477e1d940e7778e0fb0389970c21443da88a8
Trace <119770319+Goldenglows@users.noreply.github.com>
2024-04-25 23:52:34 +0800

    Add files via upload
    A truck has two fuel tanks. You are given two integers, mainTank representing the fuel present in the main tank in liters and additionalTank representing the fuel present in the additional tank in liters.
    
    The truck has a mileage of 10 km per liter. Whenever 5 liters of fuel get used up in the main tank, if the additional tank has at least 1 liters of fuel, 1 liters of fuel will be transferred from the additional tank to the main tank.
    
    Return the maximum distance which can be traveled.
    
    Note: Injection from the additional tank is not continuous. It happens suddenly and immediately for every 5 liters consumed.

---
+++ Day12.cs
@@ -0,0 +1,31 @@
+﻿public class Solution
+{
+    public int DistanceTraveled(int mainTank, int additionalTank)
+    {
+        // 初始化行驶距离
+        int distance = 0;
+
+        // 如果mainTank小于5，直接返回mainTank乘以10
+        if (mainTank < 5)
+        {
+            return mainTank * 10;
+        }
+
+        // 当mainTank大于等于5且additionalTank大于等于1
+        while (mainTank >= 5 && additionalTank >= 1)
+        {
+            // 为每5个mainTank增加行驶距离
+            distance += 5 * 10;
+            // mainTank减少5
+            mainTank -= 5;
+            // additionalTank减少1并增加给mainTank
+            additionalTank -= 1;
+            mainTank += 1;
+        }
+
+        // 处理剩下的mainTank部分
+        distance += mainTank * 10;
+
+        return distance;
+    }
+}
